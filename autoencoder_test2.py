5a80e16d8597fa3ca17dcf0d01cc6139bdc486d1
ncate <30808261+ncate@users.noreply.github.com>
2018-07-30 19:30:53 -0600

    Add files via upload

--- autoencoder_test2.py
+++ autoencoder_test2.py
@@ -4,15 +4,15 @@ import matplotlib.pyplot as plt
 import csv
 import datetime
 
-# dataPath = r"E:\School\Graduate_MS\Thesis\tfRecords\tf_NC_all_2018_7_9__H20_M38_FS33_BandsVVVH.gz" # RADAR - S1
+dataPath = r"E:\School\Graduate_MS\Thesis\Data\tfRecords\tf_NC_all_2018_7_8__H20_M42_FS33_BandsVVVH.gz" # RADAR - S1
 # dataPath = r"E:\School\Graduate_MS\Thesis\tfRecords\tf_NC_all_2018_7_11__H22_M31_FS33_BandsB11B8B4B3B2.gz"   # OPTICAL - S2
-dataPath = r"E:\School\Graduate_MS\Thesis\tfRecords\tf_NC_all_2018_7_11__H22_M24_FS33_BandsRGBN.gz"   # OPTICAL - NAIP
+# dataPath = r"E:\School\Graduate_MS\Thesis\tfRecords\tf_NC_all_2018_7_11__H22_M24_FS33_BandsRGBN.gz"   # OPTICAL - NAIP
 reader = tf.TFRecordReader()
 featureSize = 33
 batchSize = 10  # Number of samples in each batch
-epoch_num = 15    # Number of epochs to train the network
+epoch_num = 10    # Number of epochs to train the network
 lr = 0.001        # Learning rate
-batch_per_ep = 55
+batch_per_ep = 50
 
 # bands = ['VV', 'VH']
 label = 'BIOMS'
@@ -23,16 +23,10 @@ label = 'BIOMS'
 # # Dictionary with names as keys, features as values.
 # featuresDict = dict(zip(featureNames, columns))
 # # print(featuresDict)
-# features = {
-#     'BIOMS': tf.FixedLenFeature((), tf.float32),
-#     'VV': tf.FixedLenFeature((featureSize, featureSize), tf.float32),
-#     'VH': tf.FixedLenFeature((featureSize, featureSize), tf.float32)
-# }
 features = {
     'BIOMS': tf.FixedLenFeature((), tf.float32),
-    'R': tf.FixedLenFeature((featureSize, featureSize), tf.float32),
-    'G': tf.FixedLenFeature((featureSize, featureSize), tf.float32),
-    'B': tf.FixedLenFeature((featureSize, featureSize), tf.float32)
+    'VV': tf.FixedLenFeature((featureSize, featureSize), tf.float32),
+    'VH': tf.FixedLenFeature((featureSize, featureSize), tf.float32)
 }
 
 def parse_tfrecord(example_proto):
@@ -63,16 +57,16 @@ def autoencoder(inputs):
     # 16 x 16 x 32  ->  8 x 8 x 16
     # 8 x 8 x 16    ->  2 x 2 x 8
     # net = lays.conv2d(inputs, 64, [5, 5], stride=2, padding='SAME')
-    net = tf.layers.conv2d(inputs, 32, [5, 5], strides=2, padding='SAME')
-    net = tf.layers.conv2d(net, 16, [5, 5], strides=2, padding='SAME')
-    net = tf.layers.conv2d(net, 8, [5, 5], strides=4, padding='SAME')
+    net = tf.layers.conv2d(inputs = inputs, filters = 32, kernel_size = [5, 5], strides=2, padding='SAME')
+    net = tf.layers.conv2d(inputs = net, filters = 16, kernel_size = [5, 5], strides=2, padding='SAME')
+    net = tf.layers.conv2d(inputs = net, filters = 8, kernel_size = [5, 5], strides=4, padding='SAME')
     # decoder
     # 2 x 2 x 8    ->  8 x 8 x 16
     # 8 x 8 x 16   ->  16 x 16 x 32
     # 16 x 16 x 32  ->  32 x 32 x 1
-    net = tf.layers.conv2d_transpose(net, 16, [5, 5], strides=4, padding='SAME')
-    net = tf.layers.conv2d_transpose(net, 32, [5, 5], strides=2, padding='SAME')
-    net = tf.layers.conv2d_transpose(net, 1, [5, 5], strides=2, padding='SAME', activation=tf.nn.relu)
+    net = tf.layers.conv2d_transpose(inputs = net, filters = 16, kernel_size = [5, 5], strides=4, padding='SAME')
+    net = tf.layers.conv2d_transpose(inputs = net, filters = 32, kernel_size = [5, 5], strides=2, padding='SAME')
+    net = tf.layers.conv2d_transpose(inputs = net, filters = 1, kernel_size = [5, 5], strides=2, padding='SAME', activation=tf.nn.relu)
     return net
 
 
@@ -80,7 +74,7 @@ ae_inputs = tf.placeholder(tf.float32, (batchSize, 32, 32, 1))
 ae_outputs = autoencoder(ae_inputs)  # create the Autoencoder network
 # calculate the loss and optimize the network
 # loss = tf.reduce_mean(tf.square(ae_outputs - ae_inputs))  # claculate the mean square error loss
-loss = tf.losses.huber_loss(predictions = ae_outputs, labels = ae_inputs)  # claculate the mean square error loss
+loss = tf.losses.huber_loss(predictions=ae_outputs, labels=ae_inputs)  # claculate the mean square error loss
 train_op = tf.train.AdamOptimizer(learning_rate=lr).minimize(loss)
 # initialize the network
 init = tf.global_variables_initializer()
@@ -95,11 +89,17 @@ with tf.Session() as sess:
             train1, train2 = tfrecord_input_fn(fileName=dataPath, shuffle=True, batchSize=batchSize)
             a = sess.run([train1, train2])
             toFeed = np.zeros((batchSize, featureSize-1, featureSize-1, 1))
-            for i in range(len(a[0]["G"])):
-                temp = np.delete(a[0]["G"][i], featureSize-1, 0)
-                temp = np.delete(temp, featureSize-1, 1)
-                temp = np.resize(temp, (featureSize-1, featureSize-1, 1))
-                toFeed[i] = temp
+            for i in range(len(a[0]["VH"])):
+                vh = np.delete(a[0]["VH"][i], featureSize - 1, 0)
+                vh = np.delete(vh, featureSize - 1, 1)
+                vh = np.resize(vh, (featureSize - 1, featureSize - 1, 1))
+
+                vv = np.delete(a[0]["VV"][i], featureSize - 1, 0)
+                vv = np.delete(vv, featureSize - 1, 1)
+                vv = np.resize(vv, (featureSize - 1, featureSize - 1, 1))
+                # temp = np.dstack((vv, vh))
+                toFeed[i] = vh
+
             _, c = sess.run([train_op, loss], feed_dict={ae_inputs: toFeed})
             lossList.append(c)
             epList.append(ep)
@@ -108,11 +108,17 @@ with tf.Session() as sess:
     test1, test2 = tfrecord_input_fn(fileName=dataPath, shuffle=True, batchSize=batchSize)
     a = sess.run([test1, test2])
     toFeed = np.zeros((batchSize, featureSize-1, featureSize-1, 1))
-    for i in range(len(a[0]["G"])):
-        temp = np.delete(a[0]["G"][i], featureSize - 1, 0)
-        temp = np.delete(temp, featureSize - 1, 1)
-        temp = np.resize(temp, (featureSize-1, featureSize-1, 1))
-        toFeed[i] = temp
+    for i in range(len(a[0]["VH"])):
+        vh = np.delete(a[0]["VH"][i], featureSize - 1, 0)
+        vh = np.delete(vh, featureSize - 1, 1)
+        vh = np.resize(vh, (featureSize - 1, featureSize - 1, 1))
+
+        vv = np.delete(a[0]["VV"][i], featureSize - 1, 0)
+        vv = np.delete(vv, featureSize - 1, 1)
+        vv = np.resize(vv, (featureSize - 1, featureSize - 1, 1))
+        # temp = np.dstack((vv, vh))
+        toFeed[i] = vh
+
     recon_img = sess.run([ae_outputs], feed_dict={ae_inputs: toFeed})[0]
 
     # Make a csv of the loss for each batch. Do with it what you will
